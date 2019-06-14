218d25279cc9e2b477cd6709d250c59f6728457a
juanpamm <moreno.juan@correounivalle.edu.co>
2019-06-14 00:31:24 -0500

    Change of ways to construct, train and test the network

--- apps/improc/views.py
+++ apps/improc/views.py
@@ -1,8 +1,50 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.examples.tutorials.mnist import input_data
+from keras.datasets import fashion_mnist
+saver = None
+
+
+def tensorflow_test_script():
+    fashion_mnist_set = fashion_mnist
+
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist_set.load_data()
+
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+    train_images = train_images / 255.0
+
+    test_images = test_images / 255.0
+
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=5)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+    print('Test accuracy:', test_acc)
+
+    predictions = model.predict(test_images)
+
+    print(predictions[0])
+
+    print(np.argmax(predictions[0]))
+
+    print(test_labels[0])
 
 
 def apply_sigmoid_to_net(data, hidden_lay_list, lay_list):
@@ -147,6 +189,9 @@ def index(request):
 
 
 def execute_nn_training(request):
+
+    tensorflow_test_script()
+
     layers = int(request.GET.get('layers'))
     nodes = json.loads(request.GET.get('nodes'))
 

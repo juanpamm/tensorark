b032b1d01b5e2cac6743b7cdee58351a612bb373
juanpamm <moreno.juan@correounivalle.edu.co>
2019-12-08 23:07:26 -0500

    Index page
    Home page added to the project.

--- tensorark/urls.py
+++ tensorark/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.home.urls')),
     path('im_proc/', include('apps.improc.urls'))
 ]
